2288534229d54b09ed342b418deaebfe4065cae8
Runqiao Li <runqiao.li@ryerson.ca>
2022-11-29 20:31:31 -0500

    Time period query

--- routes/api/ecg.js
+++ routes/api/ecg.js
@@ -24,6 +24,50 @@ router.get('/record/by_ts/:ts', mongoChecker, async (req, res) => {
     }
 })
 
+// router.get('/record/by_ts/:ts-:tse', mongoChecker, async (req, res) => {
+//     try {
+//         const ts = +req.params.ts
+//         const tse = +req.params.ts
+//         // const ecg = await Record.findOne({'ts': ts}, "data")
+//         const ecg = await Record.find({'ts': {$gte: ts}},"data")
+//         if (!ecg) {
+//             res.status(404).send()
+//         } else {
+//             res.setHeader('Content-Type', 'application/json')
+//             res.send(JSON.stringify(ecg))
+//         }
+//     } catch (error) {
+//         console.log(error)
+//         if (isMongoError(error)) {
+//             res.status(500).send('Internal server error')
+//         } else {
+//             console.log(error)
+//             res.status(400).send('Bad Request')
+//         }
+//     }
+// })
+
+router.get('/timequery', mongoChecker, async (req, res) => {
+    console.log(req.query)
+    const max = req.query.max
+    const min = req.query.min
+    if (!max && !min) {
+        res.status(400).send('Bad Request')
+        return
+    }
+
+    let mongoose_query;
+    if (!max) {
+        mongoose_query = {'ts': {$gte: min}}
+    } else if (!min) {
+        mongoose_query = {'ts': {$lte: max}}
+    } else {
+        mongoose_query = {'ts': {$gte: min, $lte: max}}
+    }
+    const ecg = await Record.find(mongoose_query, "data ts")
+    res.send(JSON.stringify(ecg))
+})
+
 router.get('/record/all/:token', mongoChecker, async (req, res) => {
 
     if (req.params.token !== "secret token :D") {
